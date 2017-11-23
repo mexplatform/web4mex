fe12a5628db43cb12d3c0120fed65111079643fa
Igor Costa <igorsc93@gmail.com>
2017-11-23 15:26:10 -0200

    Moving project from Glassfish to Spring Boot

---
+++ src/main/java/web4mex/ExperimentController.java
@@ -0,0 +1,60 @@
+package web4mex;
+
+import org.aksw.mex.interfaces.annotations.InterfaceVersion;
+import org.aksw.mex.interfaces.annotations.Start;
+import org.aksw.mex.interfaces.annotations.algo.Algorithm;
+import org.aksw.mex.interfaces.annotations.core.*;
+import org.aksw.mex.interfaces.annotations.perf.Measure;
+import org.aksw.mex.log4mex.MEXSerializer;
+import org.aksw.mex.log4mex.MyMEX;
+import org.aksw.mex.util.MEXConstant;
+import org.aksw.mex.util.MEXEnum;
+import org.aksw.mex.log4mex.algo.AlgorithmVO;
+import org.aksw.mex.log4mex.core.HardwareConfigurationVO;
+import org.aksw.mex.log4mex.core.SamplingMethodVO;
+import org.aksw.mex.util.MEXEnum;
+
+import org.aksw.mex.log4mex.MyMEX;
+import org.aksw.mex.log4mex.MEXSerializer;
+import org.aksw.mex.interfaces.MetaGeneration;
+
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.json.JsonObject;
+import javax.json.stream.JsonParser;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ExperimentController {
+
+    private final static MyMEX mex = new MyMEX();
+
+    @PostMapping(value = {"/experimentinfo"}, consumes = {"text/plain", "application/json"}, produces = {"text/plain", "application/json"})
+    public String setExperimentInfo(@RequestBody String content){
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(content);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        System.out.println(content);
+
+        return "";
+    }
+
+    @PostMapping(value = {"/hardwareinfo"}, consumes = {"text/plain", "application/json"}, produces = {"text/plain", "application/json"})
+    public String setHardwareInfo(@RequestBody String content){
+
+        System.out.println(content);
+
+        return "";
+    }
+
+}
+
+
+
+
+
---
+++ src/main/java/web4mex/MexApplication.java
@@ -0,0 +1,12 @@
+package web4mex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MexApplication.class, args);
+    }
+}
