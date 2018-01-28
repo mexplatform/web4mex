5c544a8f5ebc8fda8c1b78258caa42be43206208
cirola2000 <cirobaroneto@gmail.com>
2018-01-28 17:53:36 +0100

    Update root controller

--- src/main/java/org/metarchive/mex/web4mex/controllers/RootController.java
+++ src/main/java/org/metarchive/mex/web4mex/controllers/RootController.java
@@ -2,13 +2,13 @@ package org.metarchive.mex.web4mex.controllers;
 
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
-	@PostMapping(value = { "/token" })
+	@GetMapping(value = { "/token" })
 	public String createToken() {
 		return UUID.randomUUID().toString();
 	}
